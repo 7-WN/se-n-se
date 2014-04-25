a94541af8b2a2e365116890e7eb76f551488fa7f
Lars De Richter <lars@anansi.be>
2014-04-25 15:54:42 +0200

    First version of Gruntfile, based on the one I wrote for the comweb.github.io repository

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+	shell: {
+	    updatePackages: {
+		command: "npm install && bower install"
+	    }
+	},
+	clean: {
+	    build: ["build"]
+	},
+	copy: {
+	    assets: {
+		expand: true,
+		cwd: "src/",
+		src: "public/**/*",
+		dest: "build/"
+	    }
+	},
+	assemble: {
+	    site: {
+		// options: {
+		//     layout: "src/layouts/page.hbs",
+		//     partials: "src/partials/**/*.hbs"
+		// },
+		// expand: true,
+		// cwd: "src/",
+		// src: "*.hbs",
+		// dest: "build/"
+	    }
+	},
+	watch: {
+	    assets: {
+		options: {
+		    livereload: true
+		},
+		files: ["src/public/**/*"],
+		tasks: ["copy:assets"]
+	    },
+	    hbs: {
+		options: {
+		    livereload: true
+		},
+		files: ["src/**/*.hbs"],
+		tasks: ["assemble"]
+	    }
+	}
+    });
+
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('assemble');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('update', "Update installed dependencies or install new dependencies.", ["shell:updatePackages"]);
+    grunt.registerTask('build', "Build a debug version of the website and put it in the build folder.", ["clean:build", "copy", "assemble"]);
+    grunt.registerTask('default', ["build"]);
+};
